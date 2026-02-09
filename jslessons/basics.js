b8a3ed001ff20dcf01a19bfe0314423575c4ecf0
pavels-s <pavels.sarkels@gmail.com>
2026-02-09 19:55:16 +0200

    New

---
+++ jslessons/basics.js
@@ -0,0 +1,21 @@
+console.log("Hello world")
+
+/*
+first 
+try
+*/
+
+let a = 4
+console.log(a)
+//var =same as = let, const
+console.log(typeof(a))
+
+let b = 234.6
+console.log(typeof(b))
+
+let c = "My String"
+console.log(c)
+
+let required = true
+console.log(typeof(required))
+
