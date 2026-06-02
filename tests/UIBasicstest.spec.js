423050a847a04debacf90d034d94a5d8c6fa680c
pavels-s <pavels.sarkels@gmail.com>
2026-06-02 15:40:43 +0300

    update

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,20 +1,25 @@
 const {test} = require('@playwright/test');
 
 
-test('Broswer context declaration', async ({browser})=>
+test.only('Broswer context declaration', async ({browser})=>
 {
     //step1 -open browser
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://www.google.com/");
 
-    //step2 -enter creds
-    //await (need to use because JS is asynchronous)
 
 });
 
 test('First Playwright test', async ({page})=>
 {
-    await page.goto("https://www.google.com/");
-    //
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    
+    console.log(await page.title());
+
+    await page.locator("#username").fill("randomuser");
+    await page.locator("[type='password']").fill("learn");
+    await page.locator("#signInBtn").click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
 });
---
+++ tests/llc.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Playwright Special locators', async ({page}) => {
+
+await page.goto("https://rahulshettyacademy.com/angularpractice/");
+await page.getByLabel("Check me out if you Love IceCreams!").click();
+await page.getByLabel("Employed").check();
+await page.getByLabel("Gender").selectOption("Female");
+
+
+});
